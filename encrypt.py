cbd628ee1563d1cea7a2cd78397eeeb7d02129e8
Manomay Kagalkar <manomay111@gmail.com>
2024-04-21 16:55:23 +0530

    completed AES + RSA file encryption and export

---
+++ encrypt.py
@@ -0,0 +1,50 @@
+import typer, os, time
+from rich import print
+from typing_extensions import Annotated
+from pathlib import Path
+from utils import get_vault_path, encrypt_file_aes, encrypt_message_rsa, compress_folder
+
+app = typer.Typer()
+
+@app.command()
+def asymmetric(
+    file: Annotated[Path, typer.Argument(..., help="path to input file")],
+    key: Annotated[Path, typer.Argument(..., help="PATH or ALIAS of public key file")],
+    alias: Annotated[Path, typer.Option(..., help="name the project or export")] = os.urandom(4).hex(),
+    algo: Annotated[str, typer.Option(help="currently supports RSA or ECC")] = "RSA",
+):
+    """
+    uses hybrid encryption on files, AES + [RSA | ECC]
+    """
+    try:
+        # get key path
+        key_path = Path(get_vault_path("keys")).joinpath(f"PUBKEY_{key}.pub")
+        if(not key_path.exists()): key_path = Path(key)
+        if(not key_path.exists()): raise typer.BadParameter(f"Key file {key} not found.")
+
+        # get output path
+        out_path = Path(get_vault_path("projects").joinpath(alias))
+        out_path.mkdir(parents=True, exist_ok=False)
+
+        # generate 32 byte key for AES
+        aes_key = os.urandom(32)
+        # encrypt the file with AES
+        encrypt_file_aes(file, aes_key, out_path)
+        print(f":white_check_mark: 1/3 saved encrypted file to {out_path}/{file.name}.enc")
+
+        # encrypt AES key with RSA
+        encrypt_message_rsa(aes_key, key_path, out_path)
+        print(f":white_check_mark: 2/3 saved encrypted AES key to {out_path}/aes.key")
+
+        # package the files for transfer
+        compress_folder(alias, out_path)
+        print(f":white_check_mark: 3/3 saved compressed archive to {os.getcwd()}")
+
+    except Exception as e:
+        print(f":no_entry: [bold red]Error:[/bold red] Could not encrypt file.\n{e}")
+        raise typer.Exit("Exited with status code 1.")
+
+
+
+if __name__ == "__main__":
+    app()
--- utils.py
+++ utils.py
@@ -1,23 +1,76 @@
-import os, platform
+import os, platform, shutil
 from pathlib import Path
-from rich import print
+from rich import print  # printing rich text
 from typer import Exit
-import typer
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.PublicKey import RSA
+from Crypto.Util.Padding import pad
+from base64 import b64encode
 
-def get_vault_path():
+# locate a managed keys directory or create one if it doesn't exist
+def get_vault_path(dir: str):
     try:
+        # get the right path based on the host OS
+        # TODO - add support for other OS
         hostOS = platform.system()
         vault_path = ""
         if hostOS == "Darwin":
-            vault_path = Path('/Users/').joinpath(os.getlogin(), '.vault', 'keys')
+            vault_path = Path('/Users/').joinpath(os.getlogin(), '.vault', dir)
         elif hostOS == "Linux":
-            vault_path = Path('/usr/').joinpath('share', 'vault', 'keys')
+            vault_path = Path('/usr/').joinpath('share', 'vault', dir)
         elif hostOS == "Windows":
-            vault_path = Path('C:/').joinpath('Program Files', 'Vault', 'keys')
+            vault_path = Path('C:/').joinpath('Program Files', 'Vault', dir)
+        # check if the path was reachable
         if(not vault_path):
             raise Exception("Insufficient file system privileges or unsupported OS. You can specify a custom path using the --path option.")
+        # create directories at the path if none exist
         vault_path.mkdir(parents=True, exist_ok=True)
         return vault_path
     except Exception as e:
         print(f":no_entry: [bold red]Error:[/bold red] Could not find or create a vault.\n{e}")
+        raise Exit("Exited with status code 1.")
+
+# encrypt a file using aes and save to output path
+def encrypt_file_aes(file: Path, key: bytes, output: Path):
+    try:
+        # prepare key and cipher
+        cipher = AES.new(key, AES.MODE_CBC)
+        # read plaintext file
+        with file.open("rb") as f:
+            data = f.read()
+            ct_bytes = cipher.encrypt(pad(data, AES.block_size))
+            ct = b64encode(ct_bytes).decode('utf-8')
+            iv = b64encode(cipher.iv).decode('utf-8')
+            # write iv + ciphertext to output file
+            out_name = f.name.split("/")[-1]
+            with open (Path(output).joinpath(f"{out_name}.enc"), 'w') as o:
+                o.write(iv + ct)
+    except Exception as e:
+        print(f":no_entry: [bold red]Error:[/bold red] Could not encrypt file.\n{e}")
+        raise Exit("Exited with status code 1.")
+
+# encrypt a key using RSA and save to output path
+def encrypt_message_rsa(message: bytes, key_path: Path, output: Path):
+    try:
+        # encrypt AES key with RSA key
+        key = RSA.import_key(open(key_path).read())
+        cipher = PKCS1_OAEP.new(key)
+        ciphertext = cipher.encrypt(message)
+        ciphertext = b64encode(ciphertext).decode("utf-8")
+        # save to key file in project path
+        with open (Path(output).joinpath("aes.key"), "w") as f:
+            f.write(ciphertext)
+    except Exception as e:
+        print(f":no_entry: [bold red]Error:[/bold red] Could not encrypt AES key.\n{e}")
+        raise Exit("Exited with status code 1.")
+
+# compress a folder and save to output path
+def compress_folder(alias: str, folder_path: Path):
+    try:
+        # create ZIP archive
+        archived = shutil.make_archive(Path(os.getcwd()).joinpath(alias), "zip", folder_path)
+        if(Path(archived).exists()): return
+        else: raise Exception("Error saving ZIP archive!")
+    except Exception as e:
+        print(f":no_entry: [bold red]Error:[/bold red] Could not compress folder.\n{e}")
         raise Exit("Exited with status code 1.")
