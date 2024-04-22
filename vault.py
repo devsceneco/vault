98609e38a0ec1cbee506d8e3f38b39f7eef37b98
Manomay Kagalkar <manomay111@gmail.com>
2024-04-22 13:08:53 +0530

    renamed keypair to keys to show that both symm and asymm keys are managed

--- vault.py
+++ vault.py
@@ -5,9 +5,10 @@ import keypair, encrypt, decrypt
 
 app = typer.Typer()
 # key management app
-app.add_typer(keypair.app, name="keypair", help="generate, store and manage asymmetric keypairs")
+app.add_typer(keypair.app, name="keys", help="generate, store and manage keys")
 # file encryption app
 app.add_typer(encrypt.app, name="encrypt", help="encrypt a file using a key")
+# file decryption app
 # app.add_typer(decrypt.app, name="decrypt", help="decrypt a file using a key")
 
 if __name__ == "__main__":
